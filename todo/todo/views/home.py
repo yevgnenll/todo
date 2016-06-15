c2ab85839333f82f0c97604a7a42f08fdd4674b5
seungkwon <yevgnenll@gmail.com>
2016-06-15 12:40:20 +0900

    첫 번째 테스트 통과
    home.py에서 view를 저렇게 쓸 수도 있구나
    그러니까 DB에 저장을 별도로 하지 않고
    새로고침을 해서 바로 값을 보여주는방식,
    이거 좋은데??

--- todo/todo/views/home.py
+++ todo/todo/views/home.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
@@ -6,4 +7,5 @@ def home(request):
     return render(
         request,
         "home.html",
+        {'new_item_text': request.POST.get('item_text')},
     )
